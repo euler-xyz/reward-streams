b17c2cef1cdb322032ba4b45ae00fb3ad25a205e
kasperpawlowski <25374117+kasperpawlowski@users.noreply.github.com>
2024-04-01 18:17:42 +0200

    fix: remappings

--- test/utils/MockController.sol
+++ test/utils/MockController.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "openzeppelin/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts/token/ERC20/ERC20.sol";
 import "evc/interfaces/IEthereumVaultConnector.sol";
 import "evc/interfaces/IVault.sol";
 
